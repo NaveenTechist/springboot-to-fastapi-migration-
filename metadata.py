ead7e6d4c82a34ad5c47f3653c8181723d2adda8
NaveenTechist <naveenyarramsetty8574@gmail.com>
2026-02-11 10:49:41 +0530

    removed dummy files

--- metadata.py
+++
@@ -1,9 +0,0 @@
-# metadata.py
-FILE_METADATA = {
-    "MONTRIAL": {
-        "tableName": "mon_trial_daily",
-        "sequenceName": "mon_trial_daily_rno_seq",
-        "procedureName": "mon_trial_load",
-        "useFilePath": False
-    }
-}
